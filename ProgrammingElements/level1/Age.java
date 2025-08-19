b08640ce1524b3fe2c12b7fc39c9cbb24760af3a
Ishika-Agrawal10 <ishikaagrawal030@gmail.com>
2025-08-19 13:01:51 +0530

    added age program file

---
+++ ProgrammingElements/level1/Age.java
@@ -0,0 +1,11 @@
+package ProgrammingElements.level1;
+
+
+public class Age {
+    public static void main(String args[]){
+        int by=2000;
+        int cy=2024;
+        int age=cy-by;
+        System.out.println("age of harry is "+ age);
+    }
+}

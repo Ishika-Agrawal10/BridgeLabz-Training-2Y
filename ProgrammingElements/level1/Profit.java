3d6780f047494595835c78feb5aa0f5b36a2e72e
Ishika-Agrawal10 <ishikaagrawal030@gmail.com>
2025-08-19 19:03:10 +0530

    [Ishika]profit and loss percent

---
+++ ProgrammingElements/level1/Profit.java
@@ -0,0 +1,12 @@
+package ProgrammingElements.level1;
+
+public class Profit {
+    public static void main(String args[]){
+        int cp=129;
+        int sp=191;
+        int profit=sp-cp;
+        double pp=((double)profit/cp)*100;
+        System.out.println("the cost price is INR "+ cp +"  and selling price is INR "+sp +"  the profit is INR "+profit+"  and the profit percentage is "+ pp);
+    }
+    
+}

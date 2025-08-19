a4ecfb645d3daeb3e65b0d134ba4a8a86cfd334e
Ishika-Agrawal10 <ishikaagrawal030@gmail.com>
2025-08-19 19:40:13 +0530

    [ishika]finding the final discounting price

--- ProgrammingElements/level1/Conversion.java
+++ ProgrammingElements/level1/Conversion.java
@@ -3,7 +3,7 @@ package ProgrammingElements.level1;
 public class Conversion {
     public static void main(String args[]){
         double d=10.8;
-        double c=1.6*10.8;
+        double c=1.6*d;
         System.out.println("The distance of 10.8 kilometre in miles is "+ c);
     }
     
--- ProgrammingElements/level1/Pen.java
+++ ProgrammingElements/level1/Pen.java
@@ -4,8 +4,8 @@ public class Pen {
     public static void main(String args[]){
         int q=14;
         int s=3;
-        int dis=14/3;
-        int nd=14%3;
+        int dis=q/s;
+        int nd=q%s;
         System.out.println("The pen per student is "+dis +" and the remaining pen not distributed is "+nd);
     }  
 }

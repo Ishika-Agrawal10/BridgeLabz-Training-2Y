4f61433416853ac08e1d22ccfdb6a8fe3aeae1dd
Ishika-Agrawal10 <ishikaagrawal030@gmail.com>
2025-08-19 18:44:52 +0530

    [Ishika]average marks of sam in pcm subject

---
+++ ProgrammingElements/level1/avgmark.java
@@ -0,0 +1,14 @@
+package ProgrammingElements.level1;
+
+public class avgmark {
+    public static void main(String args[]){
+        int physics=95;
+        int chemistry=96;
+        int maths=94;
+        double avg=(physics+chemistry+maths)/3;
+        double p=avg*100;
+        System.out.println("sam's average marks in pcm is "+p +"%");
+    }
+    }
+    
+

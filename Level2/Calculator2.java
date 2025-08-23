061ed19cadf3647cc1c2e132e0bbc972950b42d5
Ishika-Agrawal10 <ishikaagrawal030@gmail.com>
2025-08-23 11:54:44 +0530

    [ishika] performing the airthmetic operation

---
+++ Level2/Calculator2.java
@@ -0,0 +1,16 @@
+package Level2;
+import java.util.*;
+public class Calculator2 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        float number1 = sc.nextFloat();
+        float number2 = sc.nextFloat();
+        float c = number1 + number2;
+        float d = number1 - number2;
+        float e = number1 * number2;
+        float f = number1 / number2;
+        System.out.println("The addition, subtraction, multiplication and division value of 2 numbers=" + number1
+                + " and" + number2 + " is" + c + "," + d + "," + e + "," + f);
+        sc.close();
+    }
+}

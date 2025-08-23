32bfe5a6042138a682e863adea2efe4819a9bfaf
Ishika-Agrawal10 <ishikaagrawal030@gmail.com>
2025-08-23 12:02:10 +0530

    [ishika] PERIMETER OF QUARE AND ITS SIDE

---
+++ Level2/Perimeter.java
@@ -0,0 +1,13 @@
+package Level2;
+import java.util.*;
+public class Perimeter {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter  the perimeter of the square");
+        int perimeter=sc.nextInt();
+        int a=perimeter/4;
+        System.out.println("The length of the side is ___"+a+ " whose perimeter is _"+perimeter);
+                    
+        sc.close();
+    }
+}

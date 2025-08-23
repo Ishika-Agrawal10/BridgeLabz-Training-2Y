ed9e2965ea21aa4bfa14188d20fe9cfc8df5f16c
Ishika-Agrawal10 <ishikaagrawal030@gmail.com>
2025-08-23 11:41:02 +0530

    [ishika] THE TOTAL PRICE OF AN ITEM

---
+++ Item.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Item {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("eneter the quantity and unit price of an item");
+        int q= sc.nextInt();
+        int u=sc.nextInt();
+        int tp=q*u;
+        System.out.println("the total prime of an item is "+tp+" while quantity is "+q+"unit price is"+u);
+        sc.close();
+    }
+}

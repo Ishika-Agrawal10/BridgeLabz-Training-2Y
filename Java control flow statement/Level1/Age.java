a6050adc008cf74c6a9070f5a39c8ccddb71ad0c
Ishika-Agrawal10 <ishikaagrawal030@gmail.com>
2025-08-26 11:28:15 +0530

    [ishika] CHECKING WHETHER PERSON IS  ELIGIBLE OR NOT

---
+++ Java control flow statement/Level1/Age.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Age {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the age of a person:");
+        int age=sc.nextInt();
+        if(age>=18){
+            System.out.println("Eligible for vote .");
+        }
+        else{
+            System.out.println(" Not Eligible for vote");
+        }
+        sc.close();
+    }
+}

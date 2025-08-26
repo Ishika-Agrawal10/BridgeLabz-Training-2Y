ee57a749fb355ed956e41c2dfe9f2f38677bc30c
Ishika-Agrawal10 <ishikaagrawal030@gmail.com>
2025-08-26 10:53:15 +0530

    [ishika] checking whether number is divisible by 5 or not

---
+++ Java control flow statement/Level1/Divisible.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Divisible{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter a number");
+        int n=sc.nextInt();
+        if(n%5==0){
+            System.out.println("Number is divisible by 5");
+        }
+        else{
+            System.out.println("number is not divisible by 5");
+        }
+        sc.close();
+    }
+
+}

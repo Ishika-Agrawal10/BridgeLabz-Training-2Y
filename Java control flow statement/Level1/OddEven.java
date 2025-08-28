93fe590472afc476a221d8a64e5697d28b8f1e42
Ishika-Agrawal10 <ishikaagrawal030@gmail.com>
2025-08-28 19:54:50 +0530

    [ishika]EVENODD CHECKER

---
+++ Java control flow statement/Level1/OddEven.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class OddEven {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int n=sc.nextInt();
+        sc.close();
+        int i;
+        for(i=1;i<=n;i++){
+        if(i%2==0){
+            System.out.println("Even number:"+i);
+
+        }
+        else{
+            System.out.println("Odd number:"+i);
+        }
+    }
+}
+}

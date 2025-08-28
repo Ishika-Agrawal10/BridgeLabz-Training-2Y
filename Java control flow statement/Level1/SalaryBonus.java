2ac7b996d0f1f9ff202210ce0d813307fc379d16
Ishika-Agrawal10 <ishikaagrawal030@gmail.com>
2025-08-28 20:00:03 +0530

    [ISHIKA] FINDING THE BONUS OF A EMPLOYEE

---
+++ Java control flow statement/Level1/SalaryBonus.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class SalaryBonus {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the salary:");
+        int salary=sc.nextInt();
+        System.out.println("Enter the years of service:");
+        int years=sc.nextInt();
+        if(years>5){
+            int bonus=(salary*5)/100;
+            System.out.println("The bonus is:"+bonus);
+        }
+        else{
+            System.out.println("No bonus");
+        }
+        sc.close();
+    }
+    
+}

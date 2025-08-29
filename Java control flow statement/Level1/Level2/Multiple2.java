c845794ceff2053662c92ba2808bcded8439ee6d
Ishika-Agrawal10 <ishikaagrawal030@gmail.com>
2025-08-29 11:26:49 +0530

    [ishika] multiple factors using while loop

--- Java control flow statement/Level1/Level2/Multiple2.java
+++ Java control flow statement/Level1/Level2/Multiple2.java
@@ -1,20 +1,22 @@
 package Level2;
 import java.util.*;
-public class Multiple {
+public class Multiple2 {
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
-        System.out.println("enter a number:");
+        System.out.println("enter the number:");
         int n=sc.nextInt();
         sc.close();
         if(n>0 && n<100){
-            for(int i=100;i>=1;i--){
-                if(i%n == 0){
+            int i=100;
+            while(i>=1){
+                if(i%n==0){
                     System.out.println(i);
                 }
+                i--;
             }
         }
         else{
-            System.out.println("enter the positive number");
+            System.out.println("enter the positive number:");
         }
     }
 }

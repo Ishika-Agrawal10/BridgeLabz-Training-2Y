0d452edb084432c70d8963027fe44b3e06f619a7
Ishika-Agrawal10 <ishikaagrawal030@gmail.com>
2025-08-29 19:51:01 +0530

    [ishika] finding the height

---
+++ Arrays/Level1/MeanHeight.java
@@ -0,0 +1,21 @@
+package Arrays.Level1;
+import java.util.Scanner;
+public class MeanHeight {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double[] h = new double[11];
+        double sum = 0.0;
+
+        for (int i = 0; i < h.length; i++) {
+            System.out.print("Enter height of player " + (i + 1) + ": ");
+            h[i] = sc.nextDouble();
+            sum += h[i];
+        }
+
+        double mean = sum / h.length;
+        System.out.println("Mean height of the football team = " + mean);
+
+        sc.close();
+    }
+}
+

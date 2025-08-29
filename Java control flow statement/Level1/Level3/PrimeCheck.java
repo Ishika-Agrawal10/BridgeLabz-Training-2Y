d01b4e64401ef48a1cf8feda5cda5c789d686c62
Ishika-Agrawal10 <ishikaagrawal030@gmail.com>
2025-08-29 12:18:00 +0530

    [ishika] PRIME CHECKER PROGRAM

---
+++ Java control flow statement/Level1/Level3/PrimeCheck.java
@@ -0,0 +1,32 @@
+package Level3;
+import java.util.*;
+
+public class PrimeCheck {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int n = sc.nextInt();
+        sc.close();
+
+        boolean isPrime = true;  
+        if(n <= 1){
+            isPrime = false;   
+        } else {
+            
+            for(int i = 2; i < n; i++){
+                if(n % i == 0){
+                    isPrime = false;  
+                    break;            
+                }
+            }
+        }
+
+        // display result
+        if(isPrime){
+            System.out.println(n + " is a Prime Number.");
+        } else {
+            System.out.println(n + " is NOT a Prime Number.");
+        }
+    }
+}
+
